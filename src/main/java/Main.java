576f936e62615bd1aa08e96a9ca87edba515cbdb
Eric Canull <ericcanull1000@msn.com>
2017-07-06 14:01:07 -0500

    init repo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,3 @@
-
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -32,5 +30,4 @@ public class Main extends Application {
 	public static void main(String... args) {
 		launch(args);
 	}
-
 }
